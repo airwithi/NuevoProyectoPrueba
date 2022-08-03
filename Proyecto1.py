37e10529d831d1a0fd80e6cb4f72351bbce63ead
airwithi <110253813+airwithi@users.noreply.github.com>
2022-08-03 10:12:11 -0500

    agregando impresion desde rama dos

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,3 +1,5 @@
 print("Este es una impresion en pantalla hecha desde vscode")
 
 print("Este es una impresion en pantalla hecha desde github")
+
+print("Este es una impresion en pantalla hecha desde github, todo desde la rama dos")
